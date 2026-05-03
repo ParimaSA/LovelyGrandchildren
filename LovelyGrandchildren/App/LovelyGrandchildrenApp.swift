69d55fcbf721db0ffecb84bffef3ab5ddb3db6ac
ParimaSA <parima.sa@ku.th>
2026-05-04 03:45:29 +0700

    feat: initialize project

---
+++ LovelyGrandchildren/App/LovelyGrandchildrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LovelyGrandchildrenApp.swift
+//  LovelyGrandchildren
+//
+//  Created by Tho Sangsirakoup on 17/3/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct LovelyGrandchildrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            IntroView()
+        }
+    }
+}
